10131b91f01831c372306699292fb23233b806cb
Dave Martin <djtfmartin@gmail.com>
2015-07-09 14:33:07 +0100

    initial commit

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,38 @@
+grails.servlet.version = "3.0" // Change depending on target container compliance (2.5 or 3.0)
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = "target/work"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.plugin.location."bie-plugin" = "../bie-plugin"
+
+grails.project.fork = [
+        test: false,
+        run: false
+]
+
+grails.project.dependency.resolver = "maven" // or ivy
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // specify dependency exclusions here; for example, uncomment this to disable ehcache:
+        // excludes 'ehcache'
+    }
+    log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    checksums true // Whether to verify checksums on resolve
+    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
+
+    repositories {
+    }
+
+    dependencies {
+    }
+
+    plugins {
+        // plugins for the build system only
+        build ":tomcat:7.0.55"
+        compile ':cache:1.1.8'
+    }
+}
