cb10fbd75452fad7933b3b8229e87ab2d24dc42a
timo-piirmann <timurlane5@gmail.com>
2017-09-19 12:22:25 +0300

    Unify app conf

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -3,14 +3,14 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.work.dir = "target/work"
-grails.project.target.level = 1.7
-grails.project.source.level = 1.7
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
-//grails.plugin.location."bie-plugin" = "../bie-plugin"
+grails.project.target.level = 1.8
+grails.project.source.level = 1.8
 
 grails.project.fork = [
-    test: false,
-    run: false
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true], // configure settings for the test-app JVM
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256], // configure settings for the run-app JVM
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256], // configure settings for the run-war JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256] // configure settings for the Console UI JVM
 ]
 
 grails.project.dependency.resolver = "maven" // or ivy
