5954bc859e188d7c49f6ed1488e995ffc1f1f07e
timo-piirmann <timurlane5@gmail.com>
2017-11-17 13:52:00 +0200

    Unify conf files (#103)

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -6,12 +6,10 @@ grails.project.groupId = "au.org.ala" // change this to alter the default packag
 
 default_config = "/data/${appName}/config/${appName}-config.properties"
 commons_config = "/data/commons/config/commons-config.properties"
-env_config = "conf/${Environment.current.name}/Config.groovy"
 
 grails.config.locations = [
-    "file:${env_config}",
-    "file:${default_config}",
-    "file:${commons_config}"
+    "file:${commons_config}",
+    "file:${default_config}"
 ]
 
 def prop = new Properties()
@@ -23,12 +21,9 @@ try {
     prop.load(new FileInputStream(fileLocation))
     rollbarServerKey = prop.getProperty("rollbar.postServerKey") ?: ""
 } catch(IOException e) {
-    e.printStackTrace()
+    // e.printStackTrace()
 }
 
-if(!new File(env_config).exists()) {
-    println "ERROR - [${appName}] Couldn't find environment specific configuration file: ${env_config}"
-}
 if(!new File(default_config).exists()) {
     println "ERROR - [${appName}] No external configuration file defined. ${default_config}"
 }
@@ -44,21 +39,21 @@ println "[${appName}] (*) grails.config.locations = ${grails.config.locations}"
 skin.layout = "generic"
 
 // The ACCEPT header will not be used for content negotiation for user agents containing the following strings (defaults to the 4 major rendering engines)
-grails.mime.disable.accept.header.userAgents = ['Gecko', 'WebKit', 'Presto', 'Trident']
+grails.mime.disable.accept.header.userAgents = ["Gecko", "WebKit", "Presto", "Trident"]
 grails.mime.types = [ // the first one is the default format
-    all:           '*/*', // 'all' maps to '*' or the first available format in withFormat
-    atom:          'application/atom+xml',
-    css:           'text/css',
-    csv:           'text/csv',
-    form:          'application/x-www-form-urlencoded',
-    html:          ['text/html', 'application/xhtml+xml'],
-    js:            'text/javascript',
-    json:          ['application/json', 'text/json'],
-    multipartForm: 'multipart/form-data',
-    rss:           'application/rss+xml',
-    text:          'text/plain',
-    hal:           ['application/hal+json','application/hal+xml'],
-    xml:           ['text/xml', 'application/xml']
+    all:           "*/*", // "all" maps to "*" or the first available format in withFormat
+    atom:          "application/atom+xml",
+    css:           "text/css",
+    csv:           "text/csv",
+    form:          "application/x-www-form-urlencoded",
+    html:          ["text/html", "application/xhtml+xml"],
+    js:            "text/javascript",
+    json:          ["application/json", "text/json"],
+    multipartForm: "multipart/form-data",
+    rss:           "application/rss+xml",
+    text:          "text/plain",
+    hal:           ["application/hal+json","application/hal+xml"],
+    xml:           ["text/xml", "application/xml"]
 ]
 
 // URL Mapping Cache Max Size, defaults to 5000
@@ -69,30 +64,30 @@ grails.views.default.codec = "html"
 
 // The default scope for controllers. May be prototype, session or singleton.
 // If unspecified, controllers are prototype scoped.
-grails.controllers.defaultScope = 'singleton'
+grails.controllers.defaultScope = "singleton"
 
 // GSP settings
 grails {
     views {
         gsp {
-            encoding = 'UTF-8'
-            htmlcodec = 'xml' // use xml escaping instead of HTML4 escaping
+            encoding = "UTF-8"
+            htmlcodec = "xml" // use xml escaping instead of HTML4 escaping
             codecs {
-                expression = 'html' // escapes values inside ${}
-                scriptlet = 'html' // escapes output from scriptlets in GSPs
-                taglib = 'none' // escapes output from taglibs
-                staticparts = 'none' // escapes output from static template parts
+                expression = "html" // escapes values inside ${}
+                scriptlet = "html" // escapes output from scriptlets in GSPs
+                taglib = "none" // escapes output from taglibs
+                staticparts = "none" // escapes output from static template parts
             }
         }
         // escapes all not-encoded output at final stage of outputting
-        // filteringCodecForContentType.'text/html' = 'html'
+        // filteringCodecForContentType."text/html" = "html"
     }
 }
 
 
 grails.converters.encoding = "UTF-8"
 // scaffolding templates configuration
-grails.scaffolding.templates.domainSuffix = 'Instance'
+grails.scaffolding.templates.domainSuffix = "Instance"
 
 // Set to false to use the new Grails 1.2 JSONBuilder in the render method
 grails.json.legacy.builder = false
@@ -104,9 +99,9 @@ grails.spring.bean.packages = []
 grails.web.disable.multipart = false
 
 // request parameters to mask when logging exceptions
-grails.exceptionresolver.params.exclude = ['password']
+grails.exceptionresolver.params.exclude = ["password"]
 
-// configure auto-caching of queries by default (if false you can cache individual queries with 'cache: true')
+// configure auto-caching of queries by default (if false you can cache individual queries with "cache: true")
 grails.hibernate.cache.queries = false
 
 // configure passing transaction's read-only attribute to Hibernate session, queries and criterias
@@ -172,19 +167,20 @@ log4j = {
     }
 
     root {
-        error "tomcatLog", "rollbar"
+        error "rollbar"
         warn "tomcatLog"
+        info "stdout"
     }
 
-    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-           'org.codehaus.groovy.grails.web.pages',          // GSP
-           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-           'org.codehaus.groovy.grails.commons',            // core / classloading
-           'org.codehaus.groovy.grails.plugins',            // plugins
-           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+    error  "org.codehaus.groovy.grails.web.servlet",        // controllers
+           "org.codehaus.groovy.grails.web.pages",          // GSP
+           "org.codehaus.groovy.grails.web.sitemesh",       // layouts
+           "org.codehaus.groovy.grails.web.mapping.filter", // URL mapping
+           "org.codehaus.groovy.grails.web.mapping",        // URL mapping
+           "org.codehaus.groovy.grails.commons",            // core / classloading
+           "org.codehaus.groovy.grails.plugins",            // plugins
+           "org.codehaus.groovy.grails.orm.hibernate",      // hibernate integration
+           "org.springframework",
+           "org.hibernate",
+           "net.sf.ehcache.hibernate"
 }
