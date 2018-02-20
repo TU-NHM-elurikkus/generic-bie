018a72e84ca0a1eba50a7fe94f70e723b29d7bb4
timo-piirmann <timurlane5@gmail.com>
2018-02-20 16:27:02 +0200

    Minor tweaks (#111)
    * Don't log error if taxon name not found
    * Show less intimidating page in case of 404
    * Code cleanups

--- grails-app/controllers/au/org/ala/bie/SpeciesController.groovy
+++ grails-app/controllers/au/org/ala/bie/SpeciesController.groovy
@@ -23,17 +23,17 @@ class SpeciesController {
         try {
             def googleMapsKey = grailsApplication.config.googleMapsApiKey
             def url = "https://maps.googleapis.com/maps/api/geocode/json?key=${googleMapsKey}&address=" +
-                URLEncoder.encode(params.q, 'UTF-8')
+                URLEncoder.encode(params.q, "UTF-8")
             def response = new URL(url).text
             def js = new JsonSlurper()
             def json = js.parseText(response)
 
-            if(json.results){
+            if (json.results) {
                 json.results.each {
                     searchResults << [
-                            name: it.formatted_address,
-                            latitude: it.geometry.location.lat,
-                            longitude: it.geometry.location.lng
+                        name: it.formatted_address,
+                        latitude: it.geometry.location.lat,
+                        longitude: it.geometry.location.lng
                     ]
                 }
             }
@@ -48,13 +48,13 @@ class SpeciesController {
      * Search page - display search results fro the BIE (includes results for non-species pages too)
      */
     def search = {
-        def query = params.q?: "".trim()
+        def query = params.q ?: "".trim()
         if(query == "*") query = ""
-        def filterQuery = params.list('fq') // will be a list even with only one value
-        def startIndex = params.offset?:0
-        def rows = params.rows?:25
-        def sortField = params.sortField?:""
-        def sortDirection = params.dir?:"desc"
+        def filterQuery = params.list("fq") // will be a list even with only one value
+        def startIndex = params.offset ?: 0
+        def rows = params.rows ?: 25
+        def sortField = params.sortField ?: ""
+        def sortDirection = params.dir ?: "desc"
 
         if (params.dir && !params.sortField) {
             sortField = "score" // default sort (field) of "score" when order is defined on its own
@@ -68,28 +68,30 @@ class SpeciesController {
 
         // empty search -> search for all records
         if (query.isEmpty()) {
-            //render(view: '../error', model: [message: "No search term specified"])
+            //render(view: "../error", model: [message: "No search term specified"])
             query = "*:*";
         }
 
         if (filterQuery.size() > 1 && filterQuery.findAll { it.size() == 0 }) {
             // remove empty fq= params IF more than 1 fq param present
             def fq2 = filterQuery.findAll { it } // excludes empty or null elements
-            redirect(action: "search", params: [q: query, fq: fq2, start: startIndex, rows: rows, score: sortField, dir: sortDirection])
+            redirect(
+                action: "search",
+                params: [q: query, fq: fq2, start: startIndex, rows: rows, score: sortField, dir: sortDirection]
+            )
         }
 
         if (searchResults instanceof JSONObject && searchResults.has("error")) {
             log.error "TaxonConcept get Error: ${searchResults.error} | params: ${params} | ${searchResults.error}"
-            render(view: '../error', model: [message: searchResults.error])
+            render(view: "../error", model: [message: searchResults.error])
         } else {
-            render(view: 'search', model: [
+            render(view: "search", model: [
                 searchResults: searchResults?.searchResults,
                 kingdoms: kingdoms,
                 facetMap: utilityService.addFacetMap(filterQuery),
                 query: query?.trim(),
                 filterQuery: filterQuery,
                 idxTypes: utilityService.getIdxtypes(searchResults?.searchResults?.facetResults),
-                isAustralian: false,
                 collectionsMap: utilityService.addFqUidMap(filterQuery)
             ])
         }
@@ -98,7 +100,7 @@ class SpeciesController {
     /**
      * Species page - display information about the requested taxa
      *
-     * TAXON: a taxon is 'any group or rank in a biological classification in which organisms are related.'
+     * TAXON: a taxon is "any group or rank in a biological classification in which organisms are related."
      * It is also any of the taxonomic units. So basically a taxon is a catch-all term for any of the
      * classification rankings; i.e. domain, kingdom, phylum, etc.
      *
@@ -112,32 +114,27 @@ class SpeciesController {
         def taxonDetails = bieService.getTaxonConcept(guid)
 
         if (!taxonDetails) {
-            log.error "TaxonConcept Not Found: ${guid} | params: ${params}"
+            log.info("TaxonConcept Not Found: ${guid} | params: ${params}")
             response.status = 404
-            render(view: '../error', model: [message: "Requested taxon <b>" + guid + "</b> was not found"])
+            render(view: "../404", model: [message: "Requested taxon <b>${guid}</b> was not found"])
         } else if (taxonDetails instanceof JSONObject && taxonDetails.has("error")) {
             if (taxonDetails.error?.contains("FileNotFoundException")) {
-                log.error "TaxonConcept FileNotFoundException: ${guid} | params: ${params} | ${taxonDetails.error}"
+                log.info("TaxonConcept FileNotFoundException: ${guid} | params: ${params} | ${taxonDetails.error}")
                 response.status = 404
-                render(view: '../error', model: [message: "Requested taxon <b>" + guid + "</b> was not found"])
+                render(view: "../404", model: [message: "Requested taxon <b>${guid}</b> was not found"])
             } else {
-                log.error "TaxonConcept get Error: ${guid} | params: ${params} | ${taxonDetails.error}"
-                render(view: '../error', model: [message: taxonDetails.error])
+                log.info("TaxonConcept get Error: ${guid} | params: ${params} | ${taxonDetails.error}")
+                render(view: "../error", model: [message: taxonDetails.error])
             }
         } else if (taxonDetails.taxonConcept?.guid && taxonDetails.taxonConcept.guid != guid) {
             // old identifier so redirect to current taxon page
             redirect(uri: "/species/${taxonDetails.taxonConcept.guid}", permanent: true)
         } else {
-            render(view: 'show', model: [
+            render(
+                view: "show",
+                model: [
                     tc: taxonDetails,
-                    statusRegionMap: utilityService.getStatusRegionCodes(),
-                    infoSourceMap:[],
-                    textProperties: [],
                     synonyms: utilityService.getSynonymsForTaxon(taxonDetails),
-                    isAustralian: false,
-                    isRoleAdmin: false, //authService.userInRole(grailsApplication.config.auth.admin_role),
-                    userName: "",
-                    isReadOnly: grailsApplication.config.ranking.readonly,
                     sortCommonNameSources: utilityService.getNamesAsSortedMap(taxonDetails.commonNames),
                     taxonHierarchy: bieService.getClassificationForGuid(taxonDetails.taxonConcept.guid),
                     childConcepts: bieService.getChildConceptsForGuid(taxonDetails.taxonConcept.guid),
@@ -160,7 +157,7 @@ class SpeciesController {
     }
 
     def bhlSearch = {
-        render (view: 'bhlSearch')
+        render (view: "bhlSearch")
     }
 
     def soundSearch = {
---
+++ grails-app/views/404.gsp
@@ -0,0 +1,18 @@
+<html>
+    <head>
+    </head>
+
+    <body>
+        <center>
+            <h1>404</h1>
+
+            <g:if test="${message}">
+                ${message}
+            </g:if>
+            <g:else>
+                Well, stuff can go wrong sometimes and maybe you mistyped a URL, tapped/clicked on a broken link or your
+                computer just wants to drive you nuts. Anyway we are really sorry … keep calm and try again.
+            </g:else>
+        <center>
+    </body>
+</html>
